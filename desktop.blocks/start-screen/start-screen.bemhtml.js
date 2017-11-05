0a6e1ba85d57632dfe03f8391218cb5f0f582527
Alexander Melihov <amelihovv@ya.ru>
2017-11-05 22:29:37 +0300

    simplify start-screen block

--- desktop.blocks/start-screen/start-screen.bemhtml.js
+++ desktop.blocks/start-screen/start-screen.bemhtml.js
@@ -8,16 +8,3 @@ block('start-screen')(
     elem('text').tag()('p'),
     elem('header').tag()('h1')
 );
-
-block('button').match(function () {
-    return [].concat(this.ctx.mix)
-        .some(function (item) {
-            return item.block === 'start-screen' && item.elem === 'button';
-        });
-}).wrap()(function () {
-    return {
-        block: 'start-screen',
-        elem: 'button-wrapper',
-        content: applyNext()
-    };
-});
